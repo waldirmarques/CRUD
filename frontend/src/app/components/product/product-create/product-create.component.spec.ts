4111fdf6c176cd76256d5a74628c02b99fa45835
WaldirMarques <waldirmarquesdesouza@gmail.com>
2022-05-30 11:16:53 -0300

    update angular cli para versão 11

--- frontend/src/app/components/product/product-create/product-create.component.spec.ts
+++ frontend/src/app/components/product/product-create/product-create.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductCreateComponent } from './product-create.component';
 
@@ -6,7 +6,7 @@ describe('ProductCreateComponent', () => {
   let component: ProductCreateComponent;
   let fixture: ComponentFixture<ProductCreateComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProductCreateComponent ]
     })
--- frontend/src/app/components/product/product-update/product-update.component.spec.ts
+++ frontend/src/app/components/product/product-update/product-update.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductUpdateComponent } from './product-update.component';
 
@@ -6,7 +6,7 @@ describe('ProductUpdateComponent', () => {
   let component: ProductUpdateComponent;
   let fixture: ComponentFixture<ProductUpdateComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProductUpdateComponent ]
     })
